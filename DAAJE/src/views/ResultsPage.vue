d43bcfd056f0dedc63aec827a3bfc794d60a97a8
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-02-23 12:26:20 +0100

    add ResultsPage (view)

---
+++ DAAJE/src/views/ResultsPage.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="row">
+		<div class="col">
+			<h1>Results page view</h1>
+			<TheResults />
+		</div>
+	</div>
+</template>
+
+<script>
+import TheResults from '../components/TheResults.vue';
+
+
+
+</script>
