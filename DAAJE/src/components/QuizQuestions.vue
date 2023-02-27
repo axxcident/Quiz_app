5b52604beb9176f1fe9b8836c3adc9320a8c008b
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-02-27 15:24:10 +0100

    create quiz card components

---
+++ DAAJE/src/components/QuizQuestions.vue
@@ -0,0 +1,13 @@
+<template>
+	<QuizCard v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
+</template>
+
+<script setup>
+import questions from "../data/quizQuestions.json"
+import QuizCard from "./QuizCard.vue";
+import { ref } from 'vue'
+
+const quizes = ref(questions);
+</script>
+
+<style scoped></style>
