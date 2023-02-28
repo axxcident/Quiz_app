7a1f4433ead9279e5ac95762fb8818e5ff3d4da9
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-02-28 20:07:31 +0100

    add navigation and simple styling to quiz app

---
+++ DAAJE/src/views/QuizView.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<header>
+			<h5>Fråga 1/10</h5>
+			<div class="progress">
+				<div class="completion"></div>
+			</div>
+		</header>
+	</div>
+</template>
+
+<style>
+header {
+	margin: 1rem 0;
+}
+
+.progress {
+	width: 16rem;
+	height: 2rem;
+	border: 3px solid purple;
+}
+
+.completion {
+	height: 100%;
+	width: 66%;
+	background-color: purple;
+}
+</style>
