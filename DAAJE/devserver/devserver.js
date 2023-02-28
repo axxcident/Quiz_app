6d54cf0db0fb2323aa0705596c1bc4342687cee9
E.N <67209588+ElisNz@users.noreply.github.com>
2023-02-28 09:05:04 +0100

    tracked additional file

---
+++ DAAJE/devserver/devserver.js
@@ -0,0 +1,6 @@
+// === This is the index-file for all server-side scripts ===
+
+const app = require('./server.js');
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => { console.log('Running on port: ' + port); });
