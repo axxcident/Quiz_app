80853bd9c8cb411ad8154b394592b43c7a4efe85
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-02-28 12:30:41 +0100

    remove comments

--- DAAJE/src/components/QuizQuestions.vue
+++ DAAJE/src/components/QuizQuestions.vue
@@ -6,13 +6,13 @@
 import axios from 'axios'
 import QuizCard from "./QuizCard.vue";
 import { ref } from 'vue'
-// import questions from "../data/quizQuestions.json"
 
 export default {
+	components: { QuizCard },
 	async setup() {
-		const result = await axios.get('http://127.0.0.1:8080/quiz_questions/')
+		const result = await axios.get('http://localhost:8080/quiz_questions')
+		// const result = await axios.get('http://127.0.0.1:8080/quiz_questions/')
 		const quizes = ref(result.data);
-		console.log(result.data)
 		console.log(quizes.value)
 
 		return { quizes }
