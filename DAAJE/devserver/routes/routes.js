b8c840afc3c642f601423364161a205acc23b150
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-02-28 11:15:46 +0100

    change route for questions

--- DAAJE/devserver/routes/routes.js
+++ DAAJE/devserver/routes/routes.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const router  = express.Router();
-const headers = require('./headers');
-const questions = require('../public/img/data/quizQuestions.json')
+const express = require("express");
+const router = express.Router();
+const headers = require("./headers");
+const questions = require("../public/data/quizQuestions.json");
 
 // REGISTER OUR ROUTES -------------------------------
 
 // Head
-router.get('/', function(req, res, next) {
-    res.send(200, "Welcome to the API!");
-  });
+router.get("/", function (req, res, next) {
+  res.send(200, "Welcome to the API!");
+});
 /* POST default handling */
-router.post('/post', function(req, res) {
-    console.log('post recieved');
-    console.log(req.body);
-    res.json({ status: '200 OK', content: req.body });
-  });
-router.get('/quiz_questions', (req, res) => {
-  res.status('200').send(questions);
+router.post("/post", function (req, res) {
+  console.log("post recieved");
+  console.log(req.body);
+  res.json({ status: "200 OK", content: req.body });
+});
+router.get("/quiz_questions", (req, res) => {
+  res.status("200").send(questions);
 });
 // ----------------------------------------------------
 
