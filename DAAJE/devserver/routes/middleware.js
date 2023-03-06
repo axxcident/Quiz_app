ec21f11620a2c41884bed3c190c0a8948f78ca3b
E.N <67209588+ElisNz@users.noreply.github.com>
2023-03-06 16:04:22 +0100

    Moved handler functions to middleware.js - Formatted routes.js

---
+++ DAAJE/devserver/routes/middleware.js
@@ -0,0 +1,25 @@
+//	Imports
+const fs = require ("fs");
+const { v4: uuidv4 }  = require("uuid");
+
+//	Declarations
+
+const mockQuestions = fs.readFileSync("../public/data/quizQuestions.json");
+
+const mwFunctions = {
+	headers: (req, res, next) => {
+			let origin = 'http://localhost:8080';
+			res.set({'Access-Control-Allow-Origin': '*',
+					'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+					'Access-Control-Allow-Headers': 'content-type',
+					'Access-Control-Allow-Credentials': true,});
+			res.set('Access-Control-Expose-Headers', 'Access-Control-Allow-Origin');
+			console.log('response headers set');
+			next();
+		},
+	getMockQuestions: (req, res) => {
+			res.status(200).send(mockQuestions);
+		},
+};
+
+module.exports = mwFunctions;
--- DAAJE/devserver/routes/routes.js
+++ DAAJE/devserver/routes/routes.js
@@ -1,11 +1,7 @@
 const express         = require("express");
 const router          = express.Router();
-const headers         = require("./headers");
-//mock questions. Use fs to read final database
-const questions       = require("../public/data/quizQuestions.json");
-const sessionUserQuiz = require("../public/data/sessionUserQuiz.json");
-const { v4: uuidv4 }  = require("uuid");
-
+const { headers }     = require("./middleware");
+const mwFunctions     = require("./middleware");
 
 // REGISTER OUR ROUTES -------------------------------
 
@@ -13,22 +9,25 @@ const { v4: uuidv4 }  = require("uuid");
 router.get("/", function (req, res, next) {
   res.send(200, "Welcome to the API!");
 });
+
 /* POST default handling */
 router.post("/post", function (req, res) {
   console.log("post recieved");
   console.log(req.body);
   res.json({ status: "200 OK", content: req.body });
 });
-/* PUT quiz. Handler fn writes the post body into temporary database */
-router.put("/post/createquiz", (req, res) => {
+
+/* POST quiz. Handler fn writes the post body into temporary database */
+router.post("/post/create_quiz", (req, res) => {
    
 });
-/* POST edit quiz. Handler takes a quiz id as last parameter and overwrites corresponding quiz in the database */
-router.post("/post/edit")
+
+/* PUT edit quiz. Handler takes a quiz id as last parameter and overwrites corresponding quiz in the database */
+router.put("/post/edit_quiz/:id")
+
 /* GET request for quiz content */
-router.get("/quiz_questions", (req, res) => {
-  res.status(200).send(questions);
-});
+router.get("/quiz_questions", mwFunctions.getMockQuestions());
+
 /* GET request for any database folder and file */
 router.get("/:folder/:imageID", (req, res) => {
   
