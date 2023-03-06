16b5b00e510c2a7d15d137e34b93be89e32c34ed
DanielTwins <naseri.daniel24@gmail.com>
2023-03-06 22:26:53 +0100

    add UI to Quiz View

--- DAAJE/src/App.vue
+++ DAAJE/src/App.vue
@@ -1,26 +1,24 @@
 <template>
-	<RouterLink to="/">Home</RouterLink> |
-	<RouterLink to="/about">About</RouterLink> |
-	<RouterLink to="/quiz">Quiz</RouterLink> |
-	<RouterLink to="/results">Results</RouterLink> |
-	<RouterLink to="/questionspage">QuestionsPage</RouterLink>
+  <RouterLink to="/">Home</RouterLink> |
+  <RouterLink to="/about">About</RouterLink> |
+  <RouterLink to="/quiz">Quiz</RouterLink> |
+  <RouterLink to="/results">Results</RouterLink> 
 
-	<div class="container">
-		<Suspense>
-			<RouterView></RouterView>
-		</Suspense>
-	</div>
+  <div class="container">
+    <Suspense>
+      <RouterView></RouterView>
+    </Suspense>
+  </div>
 </template>
 
-<script>
-</script>
+<script></script>
 <style scoped>
 .container {
-	padding: 0 1.2rem;
-	/* extra padding + btstrap col padding*/
-	height: 100vh;
+  padding: 0 1.2rem;
+  /* extra padding + btstrap col padding*/
+  /* height: 100vh; */
 
-	background-color: #a4508b;
-	background-image: linear-gradient(286deg, #a4508b 0%, #5f0a87 84%);
+  background-color: #a4508b;
+  background-image: linear-gradient(286deg, #a4508b 0%, #5f0a87 84%);
 }
 </style>
---
+++ DAAJE/src/components/TimerComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="d-flex">
+    <img :src="clockIcon" alt="icon timer" />
+    <div class="timer">
+      <span> 45 </span>:
+      <span> 10 </span>
+    </div>
+  </div>
+</template>
+<script>
+import clockIcon from "../imgs/clock.png";
+
+export default {
+  data() {
+    return {
+      clockIcon: clockIcon,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.d-flex {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 6px;
+}
+img {
+  width: 28px;
+  height: 28px;
+}
+</style>
