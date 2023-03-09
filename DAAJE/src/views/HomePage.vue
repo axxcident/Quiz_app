4d04800e0a3136f94aba36d2ea6822b4b5e5138a
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-03-09 22:20:09 +0100

    remove heigh 100vh on main-content class styling

--- DAAJE/src/views/HomePage.vue
+++ DAAJE/src/views/HomePage.vue
@@ -6,22 +6,14 @@ import UserAvatar from "../components/UserAvatar.vue";
 <template>
   <div class="row">
     <div class="col pt-medium">
-      <UserAvatar
-        :img="imgPath"
-        :userGreeting="userGreeting"
-        :userDesc="userDesc"
-      />
+      <UserAvatar :img="imgPath" :userGreeting="userGreeting" :userDesc="userDesc" />
       <div class="hero-text">
         <h1>Welcome to your favorite Quiz App</h1>
         <h4>Increase your students performance!</h4>
       </div>
       <main class="main-content">
         <div class="card-wrapper">
-          <HomeCard
-            v-for="(option, index) in options"
-            :key="index"
-            :option="option"
-          />
+          <HomeCard v-for="(option, index) in options" :key="index" :option="option" />
         </div>
       </main>
     </div>
@@ -66,12 +58,13 @@ export default {
 .pt-medium {
   padding-top: 20px;
 }
+
 .hero-text {
   padding: 3rem 0.3rem;
   color: #d9d7d7;
 }
+
 .main-content {
-  height: 100vh;
   margin: 0 -20px;
   padding: 120px 20px 0 20px;
   border-top-left-radius: 40px;
@@ -82,6 +75,7 @@ export default {
   backdrop-filter: blur(9px);
   border: 1px solid rgba(255, 255, 255, 0.1);
 }
+
 .card-wrapper {
   display: flex;
   flex-direction: column;
