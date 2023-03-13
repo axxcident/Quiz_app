6241b8cb6a58b38329a767775a0d7bcb21284667
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-03-13 13:56:26 +0100

    show total quiz results from students as object

--- DAAJE/src/views/ResultsPage.vue
+++ DAAJE/src/views/ResultsPage.vue
@@ -1,16 +1,39 @@
 <template>
 	<div class="row">
 		<div class="col">
-			<h1>Results page view</h1>
-			<TheResults />
+			<h1>Results page For Teacher</h1>
+			<p v-for="result of sumOfResults">{{ result[19] }}</p>
+
+			<!-- går det att återanvända den här komponnent? -->
+			<TheResults :teacher="teacher" />
 		</div>
 	</div>
 </template>
 
 <script>
 import TheResults from '../components/TheResults.vue';
+import { useResultStore } from "../stores/resultStore"
 
 export default {
 	components: { TheResults },
+	data() {
+		return {
+			teacher: true
+		}
+	},
+	setup() {
+		const resultStore = useResultStore();
+
+		const sumOfResults = resultStore.fetchedResults;
+
+		return { sumOfResults }
+	}
 }
 </script>
+
+<style scoped>
+h1,
+p {
+	color: white;
+}
+</style>
