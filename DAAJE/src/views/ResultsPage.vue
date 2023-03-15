98ecc06b38a6dba4f2d06db013952c1ced666735
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-03-15 11:49:48 +0100

    get individual datapoint from sum of answer arr

--- DAAJE/src/views/ResultsPage.vue
+++ DAAJE/src/views/ResultsPage.vue
@@ -14,10 +14,6 @@ export default {
 		const sumOfResults = resultStore.fetchedResults;
 		// console.log(sumOfResults)
 
-		const sumOfResultsQuestionsArr = sumOfResults.forEach(elem => {
-			console.log(elem[0])
-		});
-
 		// const sumOfResultsQuestionsArr = sumOfResults.forEach(elem => elem.question.options.filter(option => option.isCorrect === true)[0].text)
 
 		// result.question.options.filter(option => option.isCorrect === true)[0].text
@@ -30,6 +26,23 @@ export default {
 		// 4) show the average of total results for right/wrong
 		// 5)
 
+
+		// kod från TheResults för att visa rätt svar
+		// <p>Rätt svar: {{ 
+		// result.question.options.filter(option => option.isCorrect === true)[0].label
+		// result.question.options.filter(option => option.isCorrect === true)[0].text
+
+		// Det jag vill gör nu är att sammla svars längd (mängd frågor), och antal rätt, 
+		// för att kunna räkna ut ett medeltal. 
+
+
+		// får ut datan med rätt svars alternativ på frågan i console.log
+		const sumOfResultsQuestionsArr = sumOfResults.forEach(elem => {
+			console.log(elem[0]['question']['options'].filter(option => option.isCorrect === true)[0].text)
+		});
+
+
+
 		return { sumOfResults, sumOfResultsQuestionsArr }
 	}
 }
@@ -40,7 +53,11 @@ export default {
 		<div class="col">
 			<h1>Results page For Teacher</h1>
 			<p>{{ sumOfResultsQuestionsArr }}</p>
-			<p v-for="result of sumOfResults">{{ result[19] }}</p>
+			<div v-for="(result, index) of sumOfResults">
+				<h5>Fråga {{ index + 1 }}, {{ result[index].question.text }}</h5>
+				<p>{{ result[index].question.text }}</p>
+				<p>{{ result[index].question.text }}</p>
+			</div>
 
 			<!-- går det att återanvända den här komponnent? -->
 			<TheResults :teacher="teacher" />
@@ -50,6 +67,7 @@ export default {
 
 <style scoped>
 h1,
+h5,
 p {
 	color: white;
 }
