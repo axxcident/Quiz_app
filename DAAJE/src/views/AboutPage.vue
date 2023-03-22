e0c001e56f5153a025194ab02496bb4e2f54b571
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-03-22 11:08:47 +0100

    fix styling on about page

--- DAAJE/src/views/AboutPage.vue
+++ DAAJE/src/views/AboutPage.vue
@@ -14,53 +14,55 @@ import ContactUs from "../components/ContactUs.vue";
                     </div>
                     <h1>About us</h1>
                     <h4>An independent devteam passionate about learning.</h4>
+                    <div class="textholder">
+                        <p class="about-text">
+                            Our development team is dedicated to crafting
+                            cutting-edge learning solutions that empower
+                            individuals and organizations to thrive in today's
+                            ever-evolving world. We know that continuous
+                            learning and upskilling are crucial to success, and
+                            we strive to deliver innovative technologies that
+                            enable people to learn more efficiently and
+                            effectively.
+                        <p> We
+                            believe that learning should be accessible to all,
+                            regardless of background, age, or level of
+                            expertise. That's why we design our learning apps to
+                            be intuitive and user-friendly, with clear
+                            instructions and interfaces that guide learners
+                            through each step of the process. Our aim is to make
+                            learning enjoyable, engaging, and fulfilling, so
+                            that learners remain motivated and achieve their
+                            goals.</p>
+                        <p>Our team is constantly pushing the limits of
+                            goals.</p>
+                        <p>Our team is constantly pushing the limits of
+                            what's possible in learning technology, exploring
+                            new approaches and experimenting with emerging
+                            trends. We stay at the forefront of the industry by
+                            attending conferences and workshops, and working
+                            alongside top experts to bring the latest insights
+                            and innovations to our products. Whether you're a
+                            student striving to improve your grades, an employee
+                            seeking to enhance your skills, or a company looking
+                            to train your workforce, our learning apps are the
+                            perfect tool to help you achieve your objectives.</p>
+                        </p>
+                    </div>
                 </div>
-                <div class="textholder">
-                    <p class="about-text">
-                        Our development team is dedicated to crafting
-                        cutting-edge learning solutions that empower
-                        individuals and organizations to thrive in today's
-                        ever-evolving world. We know that continuous
-                        learning and upskilling are crucial to success, and
-                        we strive to deliver innovative technologies that
-                        enable people to learn more efficiently and
-                        effectively.
-                    <p class="glitchtext"> We
-                        effectively.
-                    <p class="glitchtext"> We
-                        believe that learning should be accessible to all,
-                        regardless of background, age, or level of
-                        expertise. That's why we design our learning apps to
-                        be intuitive and user-friendly, with clear
-                        instructions and interfaces that guide learners
-                        through each step of the process. Our aim is to make
-                        learning enjoyable, engaging, and fulfilling, so
-                        that learners remain motivated and achieve their
-                        goals.</p>
-                    <p>Our team is constantly pushing the limits of
-                        goals.</p>
-                    <p>Our team is constantly pushing the limits of
-                        what's possible in learning technology, exploring
-                        new approaches and experimenting with emerging
-                        trends. We stay at the forefront of the industry by
-                        attending conferences and workshops, and working
-                        alongside top experts to bring the latest insights
-                        and innovations to our products. Whether you're a
-                        student striving to improve your grades, an employee
-                        seeking to enhance your skills, or a company looking
-                        to train your workforce, our learning apps are the
-                        perfect tool to help you achieve your objectives.</p>
-                    </p>
-                </div>
+            </main>
         </div>
+
         <ContactUs />
-        <ContactUs />
-        </main>
     </div>
-</div></template>
+</template>
 
 
 <style scoped>
+.user-avatar-filler {
+    height: 64px;
+}
+
 .pt-medium {
     padding-top: 20px;
 }
@@ -95,6 +97,8 @@ import ContactUs from "../components/ContactUs.vue";
     margin-bottom: 1rem;
 }
 
+p,
+h4,
 .about-text {
     text-align: center;
     color: #ffffff;
